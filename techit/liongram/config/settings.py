b5efd8f83d137f92af0e1d607838d464aa71125f
JSHWJ <bogum1029@gmail.com>
2023-05-18 20:09:55 +0900

    # 5차 과제

--- techit/liongram/config/settings.py
+++ techit/liongram/config/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'support',
+    'users',
+    'accounts',
+    'debug_toolbar',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -69,7 +75,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
--- techit/liongram/config/urls.py
+++ techit/liongram/config/urls.py
@@ -1,15 +1,23 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
-from posts.views import url_view, url_parameter_view,function_view,class_view
+from posts.views import index, url_view, url_parameter_view, function_view, ClassView, function_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('url/', url_view),
     path('url/<str:username>/', url_parameter_view),
+    path('fbv/list/', function_list_view),
     path('fbv/', function_view),
-    path('cbv/', class_view.as_view()),
+    path('cbv/', ClassView.as_view(), name='cbv'),
+
+    path('', index, name='index'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('posts/', include('posts.urls', namespace='posts')),
+
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
-#ctrl_shift_r : 코드 리팩터링
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
